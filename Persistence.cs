5cd85a1fde829ddd516c64ccb94f8f0d811a6716
Victor Fedotov <butjok@gmail.com>
2018-09-01 01:48:52 +0300

    first commit

---
+++ Persistence.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+static public class Persistence
+{
+	static private Dictionary<string, object> prefabs = new Dictionary<string, object>();
+
+	static public T Instantiate<T>(string json) where T : MonoBehaviour, IPersistent
+	{
+		var prefabName = Serializer.PrefabName(json);
+
+		object _prefab;
+		if (!prefabs.TryGetValue(prefabName, out _prefab) || _prefab == null)
+		{
+			_prefab = prefabs[prefabName] = Resources.Load<T>(prefabName);
+			Assert.IsNotNull(_prefab, $"cannot load prefab {prefabName}");
+		}
+
+		var prefab = (T)_prefab;
+
+		var old = prefab.gameObject.activeSelf;
+		prefab.gameObject.SetActive(false);
+		var instance = Object.Instantiate(prefab);
+		prefab.gameObject.SetActive(old);
+
+		instance.Prefab = prefab;
+		Serializer.Populate(json, instance);
+		instance.gameObject.SetActive(old);
+
+		return instance;
+	}
+}
---
+++ Serializer.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+static public class Serializer
+{
+	public const string PrefabTag = "$prefab";
+
+	static public string Serialize(object obj)
+	{
+		var jo = JObject.FromObject(obj);
+		var persistent = obj as IPersistent;
+		if (persistent != null)
+		{
+			jo[PrefabTag] = persistent.Prefab.name;
+		}
+
+		return jo.ToString(Formatting.Indented);
+	}
+
+	static public T Deserialize<T>(string json)
+	{
+		return JsonConvert.DeserializeObject<T>(json);
+	}
+
+	static public string PrefabName(string json)
+	{
+		return (string)JObject.Parse(json)[PrefabTag];
+	}
+
+	static public void Populate(string json, MonoBehaviour target)
+	{
+		JsonConvert.PopulateObject(json, target);
+	}
+}
